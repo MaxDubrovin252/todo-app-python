06667b4e09cdf129489c1b8ad4638fff70fa16c3
max <DubrovinMax1256@gmail.com>
2025-09-09 19:30:39 +0300

    add a update handler

--- api_v1/todo/views.py
+++ api_v1/todo/views.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Path, HTTPException
 from typing import Annotated
-from .schemas import TodoCreate, Todo
+from .schemas import TodoCreate, Todo, TodoUpdate
 from . import crud
 from core.models import db_helper
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -25,3 +25,9 @@ async def get_todo(todo_dep:Todo = Depends(todo_by_id), session:AsyncSession = D
 @router.get('/')
 async def get_todos(session:AsyncSession = Depends(db_helper.session_dependecy)):
     return await crud.get_all(session=session)
+
+
+@router.patch('/{product_id}')
+async def update_todo(todo_update:TodoUpdate,todo_dep:Todo = Depends(todo_by_id),session:AsyncSession = Depends(db_helper.session_dependecy)):
+    return await crud.update(session=session,todo=todo_dep, todo_update=todo_update)
+    

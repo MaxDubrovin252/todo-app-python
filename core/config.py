bdeffc74ed3ce4839a2f09475cb912bd357e6ca3
max <DubrovinMax1256@gmail.com>
2025-09-10 00:31:08 +0300

    reload a db settings

--- core/config.py
+++ core/config.py
@@ -1,15 +1,10 @@
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
-from pathlib import Path
 
-
-BASE_DIR = Path(__file__).parent.parent
-
-DB_PATH = BASE_DIR / "todo.db"
 
 
 class DBSettings(BaseModel):
-    url:str = f"sqlite+aiosqlite:///{DB_PATH}"
+    url:str = "postgresql+asyncpg://myuser:mypassword@localhost:5432/mydb"
     echo:bool = False
     
     

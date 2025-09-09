8502ee6a847699e072df1860620acb1d31667558
max <DubrovinMax1256@gmail.com>
2025-09-10 00:41:45 +0300

    refresh the depend file

--- api_v1/todo/dependencies.py
+++ api_v1/todo/dependencies.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, Path
+from fastapi import Depends, Path, HTTPException
 from . import crud
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper
@@ -7,4 +7,6 @@ from typing import Annotated
 
 
 async def todo_by_id(todo_id:Annotated[int, Path(ge=1)],session:AsyncSession = Depends(db_helper.session_dependecy)):
-    return await crud.get_by_id(session=session,id=todo_id )
+    todo = await crud.get_by_id(session=session, id=todo_id)
+    if todo is None:
+        HTTPException(status_code=404, detail=f"todo with id {todo_id} not found")

d1b835feb78e4ec9d38c37d9583eac6ae545493f
max <DubrovinMax1256@gmail.com>
2025-09-09 18:33:17 +0300

    add a router connection in main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 import uvicorn 
-
+from api_v1.todo import todo_router
 app = FastAPI()
 
+
+app.include_router(router=todo_router)
 
 
 
 if __name__=="__main__":
-    uvicorn.run("mai:app", reload=True)
+    uvicorn.run("main:app", reload=True)

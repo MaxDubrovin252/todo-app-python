26252292186d454d07a1f5b02baeb3bd80929410
max <DubrovinMax1256@gmail.com>
2025-09-09 19:15:00 +0300

    add a get all and by id handlers

--- api_v1/todo/views.py
+++ api_v1/todo/views.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path, HTTPException
+from typing import Annotated
 from .schemas import TodoCreate
 from . import crud
 from core.models import db_helper
@@ -9,8 +10,19 @@ router = APIRouter(prefix='/todo', tags=['todo'])
 
 
 
-@router.post('/create')
+@router.post('/')
 async def create_todo(todo:TodoCreate, session:AsyncSession = Depends(db_helper.session_dependecy)):
     new_todo = await crud.create(session=session, todo_in=todo)
-    return new_todo
+    return {"id":new_todo.id}
     
+    
+    
+@router.get('/{todo_id}')
+async def get_todo(todo_id:Annotated[int, Path(ge=1)], session:AsyncSession = Depends(db_helper.session_dependecy)):
+    todo = await crud.get_by_id(session=session, id=todo_id)
+    return todo
+
+
+@router.get('/')
+async def get_todos(session:AsyncSession = Depends(db_helper.session_dependecy)):
+    return await crud.get_all(session=session)

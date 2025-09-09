3f390b33671143baea45d0023d63729107dc86f3
max <DubrovinMax1256@gmail.com>
2025-09-09 17:50:53 +0300

    final the todo db schema

--- core/models/todo.py
+++ core/models/todo.py
@@ -1,2 +1,8 @@
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+class Todo(Base):
+    title:Mapped[str]= mapped_column(String(32))
+    description:Mapped[str] = mapped_column(String(128))
+    status:Mapped[str] 

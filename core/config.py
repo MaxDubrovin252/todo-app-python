d7fe9ed7438a3d813883faab5bc90ca79396c6f5
max <DubrovinMax1256@gmail.com>
2025-09-20 00:55:19 +0300

    add a wjt settings in config

--- core/config.py
+++ core/config.py
@@ -7,6 +7,10 @@ load_dotenv()
 
 DB_URL = os.getenv("DB_URL")
 
+class JWTSettings(BaseModel):
+    secret_key:str= os.getenv("SECRET_KEY")
+    exp_time:int = os.getenv("ACCESS_TOKEN_EXP")
+
 
 class DBSettings(BaseModel):
     url:str = DB_URL

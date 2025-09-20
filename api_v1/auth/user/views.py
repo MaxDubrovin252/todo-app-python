fa6bc92ec8d725560983ee308bfba73c3d6fb845
max <DubrovinMax1256@gmail.com>
2025-09-20 23:09:27 +0300

    add a sign_in logic

--- api_v1/auth/user/views.py
+++ api_v1/auth/user/views.py
@@ -4,13 +4,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper
 from . import crud
 from api_v1.auth.utils import hash_password, verify_password, create_access_token
+from .dependencies import user_verify_by_token
+
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/sign_up")
 async def sign_up(user:User, session:AsyncSession = Depends(db_helper.session_dependecy)):
     hash_pass = hash_password(password=user.password)
-    return await crud.create_user(session=session, username=user.username, password=hash_pass)
+    new_user = await crud.create_user(session=session, username=user.username, password=hash_pass)
+    if new_user is None:
+        raise HTTPException(status_code=400,detail=f"user {user.username} is already login")
+    return {"new user add":new_user.username}
 
 
 @router.post("/sign_in")
@@ -28,5 +33,3 @@ async def sign_in(user:User,session:AsyncSession = Depends(db_helper.session_dep
         raise HTTPException(status_code=400, detail="password is invalid")
     
     
-@router.get("/protected")
-async def get_all()

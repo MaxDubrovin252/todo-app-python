57792b243b9a8a15fcbc1a637fc93df62afcbd32
max <DubrovinMax1256@gmail.com>
2025-09-21 15:57:14 +0300

    add new relation

--- core/models/todo.py
+++ core/models/todo.py
@@ -1,8 +1,18 @@
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column,relationship
+from sqlalchemy import String, ForeignKey
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .user import User
+
+
 
 class Todo(Base):
     title:Mapped[str]= mapped_column(String(32))
     description:Mapped[str] = mapped_column(String(128))
     status:Mapped[str] 
+    
+    user_id:Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    
+    user:Mapped["User"] = relationship("User", back_populates="todos")
